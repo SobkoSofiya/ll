9f8fea3ed9a5a0c8ad566c6e78c117aca76d4ac8
SobkoSofiya <sobkosofiya23@gmail.com>
2021-02-09 09:45:14 +0300

    Initial Commit

---
+++ FixPrise/FixPriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixPriseApp.swift
+//  FixPrise
+//
+//  Created by Sofi on 09.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct FixPriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
